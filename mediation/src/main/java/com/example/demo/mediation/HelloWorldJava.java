3afb1ce977c313bc3802eebb2d2f51d1aa22180f
asaini <asaini@z1media.com>
2023-10-27 15:35:35 +0530

    testing code pushed

---
+++ mediation/src/main/java/com/example/demo/mediation/HelloWorldJava.java
@@ -0,0 +1,15 @@
+package com.example.demo.mediation;
+
+public class HelloWorldJava {
+
+    String myString = "HelloWorldJava";
+    int value = 0;
+    public static void main(String[] args) {
+
+    }
+
+    public void main (int a){
+        System.out.println("main int>>>>>>>>>>>>>>>>>>>>>>>>>");
+    }
+
+}
